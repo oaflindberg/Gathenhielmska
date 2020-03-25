2b052f2e11bf1e19412b4ee2a211720cc91ed73e
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-03-25 14:07:30 +0100

    Fixed events showing not showing up

--- public/themes/Gathenhielmska/page-templates/event.php
+++ public/themes/Gathenhielmska/page-templates/event.php
@@ -1,20 +1,10 @@
 <?php /* Template Name: Event */ ?>
 <?php get_header(); ?>
-<div class="">
-    <div class="">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
-</div>
+
 <?php
 $args = [
     'numberposts' => 10,
-    'post_type' => 'event',
+    'post_type' => 'Evenemang',
 ];
 ?>
 <?php $events = get_posts($args); ?>
