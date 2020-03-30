46c2c60745a93e9c6c7af1725c84cf8c2fef6853
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-03-30 15:23:34 +0200

    Started working on the contact page

--- public/themes/Gathenhielmska/page-templates/contact.php
+++ public/themes/Gathenhielmska/page-templates/contact.php
@@ -1,14 +1,13 @@
 <?php /* Template Name: Contact */ ?>
 
 <?php get_header(); ?>
-<div class="contact-form-wrapper">
-    <div class="contact-form">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <?php the_content(); ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
+<div class="contact-wrapper">
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
 </div>
 
 <?php get_footer(); ?>
