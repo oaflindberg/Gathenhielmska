7f11e9430068f947ecf7ff2c8ba4dc36d049859c
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2020-04-07 15:20:10 +0200

    category.php is working!

--- public/themes/Gathenhielmska/category.php
+++ public/themes/Gathenhielmska/category.php
@@ -5,52 +5,55 @@ Template Name: Archives
 ?>
 
 <?php get_header(); ?>
-<h1>archive</h1>
-
-<!-- <?php wp_list_categories('current-cat') ?> -->
-
 <?php
 $terms = get_terms('category');
-foreach ($terms as $term) {
-    $terms_query = new WP_Query(array(
-        'post_type' => 'evenemang',
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'category',
-                'field' => 'slug',
-                'terms' => array($term->slug),
-                'operator' => 'IN'
-            )
+$id = get_queried_object()->term_id;
+
+$terms_query = new WP_Query(array(
+    'post_type' => 'evenemang',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'category',
+            'field' => 'id',
+            'terms' => array($id),
+            'operator' => 'IN'
         )
-    ));
+    )
+));
+
 ?>
-    <h3><?php echo $term->name; ?></h3>
-    <?php if ($terms_query->have_posts()) : ?>
-        <?php while ($terms_query->have_posts()) : $terms_query->the_post(); ?>
-            <div class="">
-                <li><?php echo the_title(); ?></li>
+
+<?php if ($terms_query->have_posts()) : ?>
+    <?php while ($terms_query->have_posts()) : $terms_query->the_post(); ?>
+        <a href="<?php echo get_permalink($post); ?>">
+            <div class="event-cards">
+                <img class="thumbnail" src="<?php the_field('thumbnail') ?>" alt="">
+                <?php the_post_thumbnail('thumbnail'); ?>
+                <div>
+                    <p class="entrance"> <?php echo the_field('entrance') ?> </p>
+                </div>
+                <?php get_field('Event info') ?>
+                <div class="event-text">
+                    <h3><?php echo $post->post_title; ?></h3>
+                    <p> <?php echo the_field('date') ?> </p>
+                    <p> <?php echo the_field('descript') ?></p>
+                    <div class="ticket-wrapper">
+                        <button class="ticket-btn" href="www.biletto.se">Tickets</button>
+                        <?php $categories = get_the_terms($post, 'category')  ?>
+                        <?php foreach ($categories as $category) : ?>
+                            <a href="<?php echo get_term_link($category) ?>"> <?php echo $category->name ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
             </div>
-        <?php endwhile; ?>
-    <?php endif; ?>
+        </a>
+    <?php endwhile; ?>
+<?php endif; ?>
 <?php
-    $terms_query = null;
-    wp_reset_postdata();
-}
+$terms_query = null;
+wp_reset_postdata();
+
 ?>
 
-<?php if (is_category('teater')) : ?>
-    <h3>Teater</h3>
-<?php elseif (is_category('dans')) : ?>
-    <h3>Dans</h3>
-<?php elseif (is_category('stand-up')) : ?>
-    <h3>Stand up</h3>
-<?php elseif (is_category('hantverk')) : ?>
-    <h3>Hantverk</h3>
-<?php elseif (is_category('musik')) : ?>
-    <h3>Musik</h3>
 
-<?php else : ?>
-    <p>This is some generic text to describe all other category pages,
-        I could be left blank</p>
-<?php endif; ?>
 <?php get_footer(); ?>
