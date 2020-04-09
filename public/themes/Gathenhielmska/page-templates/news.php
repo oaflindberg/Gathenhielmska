890e816f1a3b9729fe8a541b14dcb43a7bd10bfd
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-04-09 15:25:17 +0200

    styling news page

--- public/themes/Gathenhielmska/page-templates/news.php
+++ public/themes/Gathenhielmska/page-templates/news.php
@@ -6,35 +6,54 @@ $args = [
     'numberposts' => 10,
     'post_type' => 'News',
     'orderby' => 'date',
+    'order' => 'DESC',
+];
+$argsFirst = [
+    'numberposts' => 1,
+    'post_type' => 'News',
+    'orderby' => 'date',
     'order' => 'DESC'
 ];
 ?>
 <?php $news = get_posts($args); ?>
+<?php $newsFirst = get_posts($argsFirst); ?>
 <?php if (count($news)) : ?>
+
     <div class="news-container">
-        <?php foreach ($news as $post) : ?>
+        <?php foreach ($newsFirst as $post) : ?>
             <div class="news-cards">
-                <?php if (get_field('image') != NULL) : ?>
-                    <img class="news-thumbnail" src="<?php the_field('image') ?>" alt="">
-                    <div class="news-content-container">
-                        <h3 class="news-title"><?php echo the_field('title') ?> </h3>
-                        <p class="news-short"><?php echo the_field('description') ?></p>
-                        <div class="read-more-container">
-                            <a class="read-more" href="<?php echo get_permalink($post); ?>"><button class="read-more-btn">Läs mer</button></a>
-                        </div>
+                <img class="news-thumbnail" src="<?php the_field('image') ?>" alt="">
+                <div class="news-content-container">
+                    <h3 class="news-title"><?php echo the_field('title') ?> </h3>
+                    <p class="news-short"><?php echo the_field('description') ?></p>
+                    <div class="read-more-container">
+                        <a class="read-more" href="<?php echo get_permalink($post); ?>"><button class="read-more-btn">Läs mer</button></a>
                     </div>
-                <?php else : ?>
-                    <div class="news-content-container">
-                        <h3 class="news-title"><?php echo the_field('title') ?> </h3>
-                        <p class="news-short"><?php echo the_field('description') ?></p>
-                        <div class="read-more-container">
-                            <a class="read-more" href="<?php echo get_permalink($post); ?>"><button class="read-more-btn">Läs mer</button></a>
-                        </div>
-                    </div>
-                <?php endif; ?>
+                </div>
             </div>
         <?php endforeach; ?>
-    </div>
+
+        <div class="newsletter">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                    <?php the_content(); ?>
+
+            <?php endwhile;
+            endif; ?>
+        </div>
+        <?php foreach (array_slice($news, 1) as $post) : ?>
+            <div class="news-cards">
+                <img class="news-thumbnail" src="<?php the_field('image') ?>" alt="">
+                <div class="news-content-container">
+                    <h3 class="news-title"><?php echo the_field('title') ?> </h3>
+                    <p class="news-short"><?php echo the_field('description') ?></p>
+                    <div class="read-more-container">
+                        <a class="read-more" href="<?php echo get_permalink($post); ?>"><button class="read-more-btn">Läs mer</button></a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
     </div>
 <?php endif; ?>
 <?php get_footer(); ?>
