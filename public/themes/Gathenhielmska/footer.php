cfae797548ec56dcb59d2fa670242552dc62dcac
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2020-04-14 21:30:23 +0200

    update media queries footer

--- public/themes/Gathenhielmska/footer.php
+++ public/themes/Gathenhielmska/footer.php
@@ -3,38 +3,40 @@
 <footer>
 
   <div class="footer-wrapper">
-    <div class="contact-us">
-      <div>
-        <h4>Kontakta oss!</h4>
-      </div>
-      <div>
-        <p>Stigbergstorget 7 </p>
-        <p>414 63 Göteborg</p>
-      </div>
-      <div>
-        <p>gathenhielmskabokning@gmail.com</p>
+
+    <img class="footer-img" src="<?php bloginfo('template_directory') ?>/assets/images/background/footer-img.png" alt="bird">
+
+    <div class="desktop-wrapper">
+      <div class="contact-us">
+        <div>
+          <h4>Kontakta oss!</h4>
+        </div>
+        <div>
+          <p>Stigbergstorget 7 </p>
+          <p>414 63 Göteborg</p>
+        </div>
+        <div>
+          <p>gathenhielmskabokning@gmail.com</p>
+        </div>
+
       </div>
 
+      <div class="follow-us">
+        <h4>Följ oss</h4>
+        <div class="follow-wrapper">
+          <a href="https://www.facebook.com/gathenhielmska/"> <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/facebook.svg" alt="facebook"> </a>
+          <a href="https://www.instagram.com/"> <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/instagram.svg" alt="instagram"> </a>
+          <a href="https://www.youtube.com/"> <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/youtube.svg" alt="youtube"> </a>
+        </div>
+      </div>
     </div>
-
-    <div class="follow-us">
-      <h4>Följ oss</h4>
-      <div class="follow-wrapper">
-        <a href="https://www.facebook.com/gathenhielmska/"> <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/facebook.svg" alt="facebook"> </a>
-        <a href="https://www.instagram.com/"> <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/instagram.svg" alt="instagram"> </a>
-        <a href="https://www.youtube.com/"> <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/youtube.svg" alt="youtube"> </a>
-
-      </div>
-    </div>
-
     <div class="sponsor-us">
       <h4>Sponsorer</h4>
 
       <div class="sponsor-wrapper">
-        <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/youtube.svg" alt="sponsor">
-        <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/youtube.svg" alt="sponsor">
-        <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/youtube.svg" alt="sponsor">
-        <img src="<?php bloginfo('template_directory') ?>/assets/images/icons/youtube.svg" alt="sponsor">
+        <img class="spons1" src="<?php bloginfo('template_directory') ?>/assets/images/icons/sponsor1.png" alt="sponsor">
+        <img class="spons2" src="<?php bloginfo('template_directory') ?>/assets/images/icons/sponsor2.png" alt="sponsor">
+        <img class="spons3" src="<?php bloginfo('template_directory') ?>/assets/images/icons/sponsor3.png" alt="sponsor">
 
       </div>
     </div>
